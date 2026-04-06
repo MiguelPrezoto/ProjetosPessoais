2c4c6960a7a7ba3e91276589bef6259e47d94e1c
Miguel Prezoto <prezotomiguel12@gmail.com>
2026-04-06 17:24:52 -0300

    revisao texto, popup login e linkedin
    o css do texto foi revisado e está ok, foi criado um popup quando clicado no icon de user e o link do linkedin foi arrumado.

--- marketplace/main.js
+++ marketplace/main.js
@@ -5,7 +5,12 @@ const btnDireita = document.querySelectorAll(".seta.direita");
 
 const scrollPorClique = 250;
 
-// Navegação à direita
+//popup login
+const iconUsuario = document.getElementById('iconUser');
+const loginPopup = document.getElementById('loginPopUp');
+const fecharPopUp = document.getElementById('fecharPopUp');
+
+// nav direita
 btnDireita.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     linhasProdutos[index].scrollBy({
@@ -15,7 +20,7 @@ btnDireita.forEach((btn, index) => {
   });
 });
 
-// Navegação à esquerda
+// nav esquerda
 btnEsquerda.forEach((btn, index) => {
   btn.addEventListener("click", () => {
     linhasProdutos[index].scrollBy({
@@ -23,4 +28,23 @@ btnEsquerda.forEach((btn, index) => {
       behavior: "smooth",
     });
   });
+});
+
+
+
+// abrir popup
+iconUsuario.addEventListener('click', () => {
+    loginPopup.style.display = 'flex'; // Exibe o popup
+});
+
+// fechar popup
+fecharPopUp.addEventListener('click', () => {
+    loginPopup.style.display = 'none'; // Esconde o popup
+});
+
+// Fechar o popup se o usuário clicar fora da caixa de conteúdo
+window.addEventListener('click', (event) => {
+    if (event.target === loginPopup) {
+        loginPopup.style.display = 'none'; // Esconde o popup se clicar fora dele
+    }
 });

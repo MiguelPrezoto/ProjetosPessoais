d257dc567f8c4bff6bfcea18cce93551eb9c101a
Miguel Prezoto <prezotomiguel12@gmail.com>
2026-04-28 14:09:59 -0300

    Revert "Merge pull request #2 from MiguelPrezoto/projetoReactNative"
    This reverts commit 4aae83077397094bde869b6a6481b25d439837bc, reversing
    changes made to 2dddc28ef8c0f74c283265dfa3b39fdc05614d3f.

--- TesteNavigationTSX/app/(tabs)/tela1.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native"
-
-export default function Tela1() {
-    return(
-        <View>
-            <Text> Tela 1 </Text>
-        </View>
-    )
-}
--- TesteNavigationTSX/app/(tabs)/tela2.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native"
-
-export default function Tela2() {
-    return(
-        <View>
-            <Text> Tela 2 </Text>
-        </View>
-    )
-}
